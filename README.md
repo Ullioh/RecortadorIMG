2a768e3bfb9f2c6c37210ccc4b9acf30da50e95e
Julio Linarez <julio.lanzerz@gmail.com>
2024-10-07 23:13:00 -0400

    por ahora solo sera uno

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RecortadorIMG
+
 little scrip to cut images in osba
+
+## como usar
+
+### 1 se necesita tener instalado el python
+### 2 se tiene que instalar el pillow
+
+se ejecuta con el python es facil.
---
+++ Recortador.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from PIL import Image
+
+# ruta de la carpeta donde están las imágenes originales
+carpeta_origen = "C:/Users/Hidari/Downloads/images" #bucar en la pc
+carpeta_destino = "C:/Users/Hidari/Downloads/images/recortadas" #bucar en la pc y puedes poner el nombre que quieras dondes dice "recortadas"
+
+# se crea la carpeta de destino lo mas probable e sque no exisra
+if not os.path.exists(carpeta_destino):
+    os.makedirs(carpeta_destino)
+
+# como se va a recortar va en ese orden (izquierda, arriba, derecha, abajo)
+area_a_recortar = (100, 662, 1436, 1496)
+
+# busca todas las imagenes, si es un formato dufernete se deben cambiar.
+imagenes = glob(os.path.join(carpeta_origen, "*.png"))
+
+for imagen_path in imagenes:
+    # abre la imagen
+    imagen = Image.open(imagen_path)
+    
+    # recorta la imagen
+    imagen_recortada = imagen.crop(area_a_recortar)
+    
+    # toma el nombre de archivo original
+    nombre_archivo = os.path.basename(imagen_path)
+    
+    # guarda la imagen recortada en la carpeta de destino con el mismo nombre
+    imagen_recortada.save(os.path.join(carpeta_destino, nombre_archivo))
+#cierra el ciclo del programa para que no haya ni errores ni conflictos
+sys.exit()
